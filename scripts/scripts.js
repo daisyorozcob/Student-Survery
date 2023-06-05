4875793bb94347ba841aaaa6770f992a8d7800f6
daisyorozcob <daisyyy.ob@gmail.com>
2023-06-04 23:28:40 -0700

    added to java

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResults();
+    const  = parseInt(document.querySelector("").value);
+    const  = parseInt(document.querySelector("").value);
+
+    // we're evaluating the Tower of Doom ride first
+    if (>=  && >= ) {
+      document.getElementById("").removeAttribute("");
+      document.getElementById("").removeAttribute("");
+      document.getElementById("").removeAttribute("");
+    } 
+  };
